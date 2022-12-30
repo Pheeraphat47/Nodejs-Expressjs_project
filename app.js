5b2f26bf90bfd05fb033bd0f96fa4cc673edc28e
P <dpheeraphat2547@gmail.com>
2022-12-30 21:15:51 +0700

    test topstar route page

--- app.js
+++ app.js
@@ -5,20 +5,31 @@ const chalk = require('chalk');
 const debug = require('debug')('app'); // เรียกใช้ debug
 const morgan = require('morgan'); // เรียกใช้ morgan
 const path = require('path');
-
+const topstarsRouter = express.Router(); // สร้าง Router
 
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")))
+app.use(express.static(path.join(__dirname, "/public/")))
 
-app.set("views","./src/views");
-app.set("view engine","ejs")
+app.set("views", "./src/views");
+app.set("view engine", "ejs")
 
-app.get("/",(req,res)=>{
+
+topstarsRouter.route("/").get((req, res) => {
+    res.send("This is Topstar Page");
+});
+
+topstarsRouter.route("/1").get((req, res) => {
+    res.send("This is Topstar Page 1");
+});
+
+app.use("/topstars", topstarsRouter)
+
+app.get("/", (req, res) => {
     res.render('index');
-}) 
+})
 
 // รับ port
-app.listen(port,() =>{
+app.listen(port, () => {
     debug("Listening on port%d" + chalk.red(" : " + port));
 })
