794d4ed9e0ab53dd5865fdfad9fa0a0cec340aa5
P <dpheeraphat2547@gmail.com>
2022-12-30 17:22:34 +0700

    install chalk debug morgan

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express'); // เรียกใช้ expressjs
 const app = express();
 const port = 3000; // ระบุ port
+const chalk = require('chalk');
+const debug = require('debug')('app'); // เรียกใช้ debug
+const morgan = require('morgan'); // เรียกใช้ morgan
+
 
 
 app.get("/",(req,res) => {
    res.send('Hello World!');
+})
+
+// รับ port
+app.listen(port,() =>{
+    debug("Listening on port%d" + chalk.red(" : " + port));
 })
