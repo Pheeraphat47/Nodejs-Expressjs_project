ae49ab0389201c223671e2349d36914e9bbab096
P <dpheeraphat2547@gmail.com>
2022-12-30 20:26:12 +0700

    add css and js to public

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const port = 3000; // ระบุ port
 const chalk = require('chalk');
 const debug = require('debug')('app'); // เรียกใช้ debug
 const morgan = require('morgan'); // เรียกใช้ morgan
+const path = require('path');
 
 
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public")))
 
-
+app.get("/",(req,res) => {
+   res.send('Hello World!');
+})
 
 // รับ port
 app.listen(port,() =>{
