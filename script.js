ef15402761b216102e36819c49a519ce910b7de5
P <dpheeraphat2547@gmail.com>
2022-12-30 19:00:01 +0700

    finish responsive navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger_menu = document.querySelector(".hamburger_menu");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger_menu.addEventListener("click", () => {
+    hamburger_menu.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
