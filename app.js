34924fd666ed114aff98548c5e64baeb4e53267b
P <dpheeraphat2547@gmail.com>
2022-12-30 20:57:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ app.use(express.static(path.join(__dirname,"/public/")))
 app.set("views","./src/views");
 app.set("view engine","ejs")
 
-app.get("/",(req,res) => {
-   res.render('Hello World!');
-})
+
 
 // รับ port
 app.listen(port,() =>{
