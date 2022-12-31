92453b5cc9f48fdd2e7193ab70ad6d013c6d0282
P <dpheeraphat2547@gmail.com>
2022-12-31 15:33:50 +0700

    test setup routing finish

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const chalk = require('chalk');
 const debug = require('debug')('app'); // เรียกใช้ debug
 const morgan = require('morgan'); // เรียกใช้ morgan
 const path = require('path');
-const topstarsRouter = express.Router(); // สร้าง Router
-
-
+const topstarsRouter = express.Router(); // สร้าง Router ของหน้า topstars
+const winnersRouter = express.Router(); // สร้าง Router ของหน้า winners
+const homeRouter = express.Router(); // สร้าง Router ของหน้า home
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")))
 
@@ -15,19 +15,36 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs")
 
 
+// route ไปหน้า topstars
 topstarsRouter.route("/").get((req, res) => {
-    res.send("This is Topstar Page");
+    res.render('topstars');
 });
 
-topstarsRouter.route("/1").get((req, res) => {
-    res.send("This is Topstar Page 1");
-});
+// route ไปหน้า winners
+winnersRouter.route("/").get((req,res) =>{
+    res.render('winners')
+})
 
+// route ไปหน้า home
+homeRouter.route("/").get((req,res) => {
+    res.render('index'); // ใส่ชื่อไฟล์ ซึ่งหน้า home ของเราชื่อ index 
+})
+
+//  สร้าง Topstars Router
 app.use("/topstars", topstarsRouter)
+
+// สร้าง winners Router
+app.use("/winners",winnersRouter)
+
+// สร้าง home Router
+app.use("/home",homeRouter)
+
 
 app.get("/", (req, res) => {
     res.render('index');
 })
+
+
 
 // รับ port
 app.listen(port, () => {
